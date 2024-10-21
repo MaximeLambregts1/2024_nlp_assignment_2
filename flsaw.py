d7a177675ad353afceb30ef4f97e8623400c9cf5
20182877 <m.p.n.m.lambregts@student.tue.nl>
2024-10-21 17:04:31 +0200

    FLSA-W first iteration gemaakt. Eerst meerdere topic hoeveelheden gemaakt. 10 topics als uitgangspunt genomen omdat die dan nog het meeste woorden pakt die relevant zijn, daaruit de onnodige gefilterd. Nu op naar iteratie twee om zinloze en typos eruit te filteren

--- flsaw.py
+++ flsaw.py
@@ -10,23 +10,49 @@ from nltk.corpus import stopwords
 stop_words = set(stopwords.words('english'))
 from collections import Counter
 
-#import data
-# import data
-FILE_PATH = 'data/nvidia_articles.csv'
-data = pd.read_csv(FILE_PATH,
-                   usecols=['content'],
-                   )['content'].tolist()
+# import data, source: https://towardsdatascience.com/fuzzytm-a-python-package-for-fuzzy-topic-models-fd3c3f0ae060
+df = pd.read_csv('data/nvidia_articles.csv', converters={'content': literal_eval})
 
-flsaW = FLSA_W(input_file = data,
-               num_topics=10,
-               num_words=10,
-               )
+#Convert data to list of lists
+data = df['content'].values.tolist()
 
-flsaW.get_vocabulary_size()
-pwgt, ptgd = flsaW.get_matrices() # THIS TRAINS THE MODEL
-print(flsaW.show_topics())
-for topic in flsaW.show_topics(representation='words'):
-    print(topic)
-print(flsaW.get_coherence_score())
-print(flsaW.get_diversity_score())
-print(flsaW.get_interpretability_score())
+#Create word counter
+all_words = [word for doc in data for word in doc]
+word_freq = Counter(all_words)
+
+
+#First iteration of making sense
+nr_of_topics = list(range(1,11))
+dict_coherence_score = {}
+topic_dict = {}
+
+for i in nr_of_topics:
+    flsaW = FLSA_W(input_file=data,
+                   num_topics=i,
+                   num_words=10,
+                   )
+    flsaW.get_vocabulary_size()
+    pwgt, ptgd = flsaW.get_matrices()
+    topic_dict[i] = {}
+
+    topics = flsaW.show_topics(representation='words')  # haal de topics op in woordrepresentatie
+
+    # Loop door de topics en sla ze op
+    for topic_num, topic in enumerate(topics):
+        topic_dict[i][topic_num] = topic  # sla elk topic en bijbehorende woorden op
+
+    dict_coherence_score[i] = flsaW.get_coherence_score()
+
+print(dict_coherence_score)
+print(topic_dict)
+print(topic_dict[10])
+
+
+#First iteration of word removal
+words_to_remove = ['also', 'p', 'whileolympusshares', 'upnoteus', 'foo', 'chee']
+# Check frequency of words
+word_counts = {word: word_freq[word] for word in words_to_remove}
+
+print("Word frequencies of specific words:", word_counts)
+
+data_iteration_2 = [[word for word in document if word not in words_to_remove] for document in data]
